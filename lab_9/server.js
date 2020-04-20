355741bb02a34edfba8e67a92e49a01a72e5c5c1
Shayon <shayonz96@gmail.com>
2020-04-19 23:07:36 -0400

    Update server.js

--- lab_9/server.js
+++ lab_9/server.js
@@ -6,7 +6,6 @@ const fetch = require('node-fetch');
 // Here we instantiate the server we're going to turn on
 const app = express();
 
-
 // Servers are often subject to the whims of their environment.
 // Here, if our server has a PORT defined in its environment, it will use that.
 // Otherwise, it will default to port 3000
@@ -19,86 +18,84 @@ app.use(express.json());
 // And the ability to serve some files publicly, like our HTML.
 app.use(express.static('public'));
 
-
-
 function processDataForFrontEnd(req, res) {
   const baseURL = 'https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json'; // Enter the URL for the data you would like to retrieve here
 
   // Your Fetch API call starts here
   // Note that at no point do you "return" anything from this function -
   // it instead handles returning data to your front end at line 34.
-    fetch(baseURL)
-      .then((resuts) => results.json())
-      //Process code here
-      .then((data) => data.json()) // this is an "implicit return" - we're returning the results of the Fetch request to the next step.
-        .then((data) => {
-          // this is an explicit return. If I want my information to go further, I'll need to use the "return" keyword before the brackets close
-          console.log(data);
-          const clearEmptyData = data.filter((f) => f.geocoded_column_1);
-          const refined = clearEmptyData.map((m) => ({
-            category: m.category,
-            name: m.name,
-            latLong: m.geocoded_column_1.coordinates,
-          }));
-          // return data; // <- this will pass the data to the next "then" statement when I'm ready.
-          return refined;
-        })
-        .then((data) => {
-          return data.reduce((result, current) => {
-            if (!result[current.category]) {
-              //c.categories.push(current.category)
-              result[current.category] = [];
-              // {"Carry-out": []}
-            }
-            result[current.category].push(current);
-            return result;
-          }, {});
-        })
-        .then((data) => {
-          console.log("new data", data);
-          const reformattedData = Object.entries(data).map((current, i) => {
-            console.log(current);
-            return {
-              y: current[1].length,
-              label: current[0],
-            };
-          });
-          return reformattedData;
-        })
-        .then((results) => {
-          console.log(results);
-          //https://canvasjs.com/javascript-charts/pie-chart-custom-radius/
-          const chart = new CanvasJS.Chart("chartContainer", {
-            theme: "light2",
-            exportEnaabled: true,
-            animationEnabled: true,
-            title: {
-              text: "Let's Find Somwhere To Eat",
-            },
-            data: [
-              {
-                type: "pie",
-                IndexLabelFontSize: 16,
-                indexLabel: "{label} - {y}",
-                dataPoints: results,
-              },
-            ],
-          });
-          chart.render();
-        });
+  fetch(baseURL)
+    .then((resuts) => results.json())
+    //Process code here
+    .then((data) => data.json()) // this is an "implicit return" - we're returning the results of the Fetch request to the next step.
     .then((data) => {
-        console.log(data);
-        res.send({ data: data }); // here's where we return data to the front end
-      })
-      .catch((err) => {
-        console.log(err);
-        res.redirect('/error');
+      // this is an explicit return. If I want my information to go further, I'll need to use the "return" keyword before the brackets close
+      console.log(data);
+      const clearEmptyData = data.filter((f) => f.geocoded_column_1);
+      const refined = clearEmptyData.map((m) => ({
+        category: m.category,
+        name: m.name,
+        latLong: m.geocoded_column_1.coordinates,
+      }));
+      // return data; // <- this will pass the data to the next "then" statement when I'm ready.
+      return refined;
+    })
+    .then((data) => {
+      return data.reduce((result, current) => {
+        if (!result[current.category]) {
+          result[current.category] = [];
+        }
+        result[current.category].push(current);
+        return result;
+      }, {});
+    })
+    .then((data) => {
+      console.log('new data', data);
+      const reformattedData = Object.entries(data).map((current, i) => {
+        console.log(current);
+        return {
+          y: current[1].length,
+          label: current[0],
+        };
       });
+      return reformattedData;
+    })
+    .then((results) => {
+      console.log(results);
+      //https://canvasjs.com/javascript-charts/pie-chart-custom-radius/
+      const chart = new CanvasJS.Chart('chartContainer', {
+        theme: 'light2',
+        exportEnaabled: true,
+        animationEnabled: true,
+        title: {
+          text: 'Lets Find Somwhere To Eat',
+        },
+        data: [
+          {
+            type: 'pie',
+            IndexLabelFontSize: 16,
+            indexLabel: '{label} - {y}',
+            dataPoints: results,
+          },
+        ],
+      });
+      chart.render();
+    })
+    .then((data) => {
+      console.log(data);
+      res.send({ data: data }); // here's where we return data to the front end
+    })
+    .catch((err) => {
+      console.log(err);
+      res.redirect("/error");
+    });
 }
 
 // This is our first route on our server.
 // To access it, we can use a "GET" request on the front end
 // by typing in: localhost:3000/api or 127.0.0.1:3000/api
-app.get('/api', (req, res) => {processDataForFrontEnd(req, res)});
+app.get('/api', (req, res) => {
+  processDataForFrontEnd(req, res);
+});
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
