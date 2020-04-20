4df90f741e0a0f2f2164630f7362bb78d1cadbba
Shayon <shayonz96@gmail.com>
2020-04-19 22:44:37 -0400

    work

--- lab_9/server.js
+++ lab_9/server.js
@@ -22,14 +22,71 @@ app.use(express.static('public'));
 
 
 function processDataForFrontEnd(req, res) {
-  const baseURL = ''; // Enter the URL for the data you would like to retrieve here
+  const baseURL = 'https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json'; // Enter the URL for the data you would like to retrieve here
 
   // Your Fetch API call starts here
   // Note that at no point do you "return" anything from this function -
   // it instead handles returning data to your front end at line 34.
     fetch(baseURL)
-      .then((r) => r.json())
-      .then((data) => {
+      .then((resuts) => results.json())
+      //Process code here
+      .then((data) => data.json()) // this is an "implicit return" - we're returning the results of the Fetch request to the next step.
+        .then((data) => {
+          // this is an explicit return. If I want my information to go further, I'll need to use the "return" keyword before the brackets close
+          console.log(data);
+          const clearEmptyData = data.filter((f) => f.geocoded_column_1);
+          const refined = clearEmptyData.map((m) => ({
+            category: m.category,
+            name: m.name,
+            latLong: m.geocoded_column_1.coordinates,
+          }));
+          // return data; // <- this will pass the data to the next "then" statement when I'm ready.
+          return refined;
+        })
+        .then((data) => {
+          return data.reduce((result, current) => {
+            if (!result[current.category]) {
+              //c.categories.push(current.category)
+              result[current.category] = [];
+              // {"Carry-out": []}
+            }
+            result[current.category].push(current);
+            return result;
+          }, {});
+        })
+        .then((data) => {
+          console.log("new data", data);
+          const reformattedData = Object.entries(data).map((current, i) => {
+            console.log(current);
+            return {
+              y: current[1].length,
+              label: current[0],
+            };
+          });
+          return reformattedData;
+        })
+        .then((results) => {
+          console.log(results);
+          //https://canvasjs.com/javascript-charts/pie-chart-custom-radius/
+          const chart = new CanvasJS.Chart("chartContainer", {
+            theme: "light2",
+            exportEnaabled: true,
+            animationEnabled: true,
+            title: {
+              text: "Let's Find Somwhere To Eat",
+            },
+            data: [
+              {
+                type: "pie",
+                IndexLabelFontSize: 16,
+                indexLabel: "{label} - {y}",
+                dataPoints: results,
+              },
+            ],
+          });
+          chart.render();
+        });
+    .then((data) => {
         console.log(data);
         res.send({ data: data }); // here's where we return data to the front end
       })
